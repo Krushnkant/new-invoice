3a5d6ff0532c4dc88d432ab1326066e7fa6fc36b
Krushnkant Vadadoriya <krushnvadadoriya@gmail.com>
2024-04-09 13:37:41 +0530

    Amount Digit to Words Convert

--- app/helpers/CommonHelper.php
+++ app/helpers/CommonHelper.php
@@ -1,54 +1,13 @@
 <?php
-
-function numberTowords($number) {
-    // $no = floor($number);
-    // $point = round($number - $no, 2) * 100;
-    // $hundred = null;
-    // $digits_1 = strlen($no);
-    // $i = 0;
-    // $str = array();
-    // $words = array(
-    //     '0' => '', '1' => 'one', '2' => 'two',
-    //     '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
-    //     '7' => 'seven', '8' => 'eight', '9' => 'nine',
-    //     '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
-    //     '13' => 'thirteen', '14' => 'fourteen',
-    //     '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen',
-    //     '18' => 'eighteen', '19' =>'nineteen', '20' => 'twenty',
-    //     '30' => 'thirty', '40' => 'forty', '50' => 'fifty',
-    //     '60' => 'sixty', '70' => 'seventy',
-    //     '80' => 'eighty', '90' => 'ninety');
-    // $digits = array('', 'hundred', 'thousand', 'lakh', 'crore');
-    // while ($i < $digits_1) {
-    //     $divider = ($i == 2) ? 10 : 100;
-    //     $number = floor($no % $divider);
-    //     $no = floor($no / $divider);
-    //     $i += ($divider == 10) ? 1 : 2;
-    //     if ($number) {
-    //         $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
-    //         $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
-    //         $str [] = ($number < 21) ? $words[$number] .
-    //             " " . $digits[$counter] . $plural . " " . $hundred
-    //             :
-    //             $words[floor($number / 10) * 10]
-    //             . " " . $words[$number % 10] . " "
-    //             . $digits[$counter] . $plural . " " . $hundred;
-    //     } else {
-    //         $str[] = null;
-    //     }
-    // }
-    // $str = array_reverse($str);
-    // $result = implode('', $str);
-    // $points = ($point) ?
-    //     "" . $words[$point / 10] . " " .
-    //     $words[$point = $point % 10] : '';
-    // // $rettxt = $result . "Rupees Only";// . $points . " Paise";
-    // $rettxt = $result . "Rupees  " . $points . " Paisa";
-    // return ucwords($rettxt);
-
+function numberTowordsForPaisa($number) {
     $no = floor($number);
     $point = round($number - $no, 2) * 100;
-    $words = array('0' => '', '1' => 'one', '2' => 'two',
+    $hundred = null;
+    $digits_1 = strlen($no);
+    $i = 0;
+    $str = array();
+    $words = array(
+        '0' => '', '1' => 'one', '2' => 'two',
         '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
         '7' => 'seven', '8' => 'eight', '9' => 'nine',
         '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
@@ -58,41 +17,78 @@ function numberTowords($number) {
         '30' => 'thirty', '40' => 'forty', '50' => 'fifty',
         '60' => 'sixty', '70' => 'seventy',
         '80' => 'eighty', '90' => 'ninety');
-    $digits = array('', '', 'hundred', 'thousand', 'lakh', 'crore');
-    
-    $number = explode(".", $number);
-    $result = array("","");
-    $j =0;
-    foreach($number as $val){
-        // loop each part of number, right and left of dot
-        for($i=0;$i<strlen($val);$i++){
-            // look at each part of the number separately  [1] [5] [4] [2]  and  [5] [8]
-            
-            $numberpart = str_pad($val[$i], strlen($val)-$i, "0", STR_PAD_RIGHT); // make 1 => 1000, 5 => 500, 4 => 40 etc.
-            if($numberpart <= 20){
-                $numberpart = 1*substr($val, $i,2);
-                $i++;
-                $result[$j] .= $words[$numberpart] ." ";
-            }else{
-                //echo $numberpart . "<br>\n"; //debug
-                if($numberpart > 90){  // more than 90 and it needs a $digit.
-                    $result[$j] .= $words[$val[$i]] . " " . $digits[strlen($numberpart)-1] . " "; 
-                }else if($numberpart != 0){ // don't print zero
-                    $result[$j] .= $words[str_pad($val[$i], strlen($val)-$i, "0", STR_PAD_RIGHT)] ." ";
-                }
-            }
+    $digits = array('', 'hundred', 'thousand', 'lakh', 'crore');
+    while ($i < $digits_1) {
+        $divider = ($i == 2) ? 10 : 100;
+        $number = floor($no % $divider);
+        $no = floor($no / $divider);
+        $i += ($divider == 10) ? 1 : 2;
+        if ($number) {
+            $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+            $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+            $str [] = ($number < 21) ? $words[$number] .
+                " " . $digits[$counter] . $plural . " " . $hundred
+                :
+                $words[floor($number / 10) * 10]
+                . " " . $words[$number % 10] . " "
+                . $digits[$counter] . $plural . " " . $hundred;
+        } else {
+            $str[] = null;
         }
-        $j++;
     }
-    if(trim($result[0]) != "") {
-        $amountWord = $result[0] . "Rupees ";
-    }
-    if($result[1] != "" && $point){
-        $amountWord .= $result[1] . "Paisa";
-    } 
-    $amountWord .= " Only";
+    $str = array_reverse($str);
+    $result = implode('', $str);
+    $rettxt = $result . "Paisa";
+    return ucwords($rettxt);
+}
 
-    return $amountWord;
+function numberTowords($number) {
+    $no = floor($number);
+    $point = round($number - $no, 2) * 100;
+    $hundred = null;
+    $digits_1 = strlen($no);
+    $i = 0;
+    $str = array();
+    $words = array(
+        '0' => '', '1' => 'one', '2' => 'two',
+        '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
+        '7' => 'seven', '8' => 'eight', '9' => 'nine',
+        '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
+        '13' => 'thirteen', '14' => 'fourteen',
+        '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen',
+        '18' => 'eighteen', '19' =>'nineteen', '20' => 'twenty',
+        '30' => 'thirty', '40' => 'forty', '50' => 'fifty',
+        '60' => 'sixty', '70' => 'seventy',
+        '80' => 'eighty', '90' => 'ninety');
+    $digits = array('', 'hundred', 'thousand', 'lakh', 'crore');
+    while ($i < $digits_1) {
+        $divider = ($i == 2) ? 10 : 100;
+        $number = floor($no % $divider);
+        $no = floor($no / $divider);
+        $i += ($divider == 10) ? 1 : 2;
+        if ($number) {
+            $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+            $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+            $str [] = ($number < 21) ? $words[$number] .
+                " " . $digits[$counter] . $plural . " " . $hundred
+                :
+                $words[floor($number / 10) * 10]
+                . " " . $words[$number % 10] . " "
+                . $digits[$counter] . $plural . " " . $hundred;
+        } else {
+            $str[] = null;
+        }
+    }
+    $str = array_reverse($str);
+    $result = implode('', $str);
+    $pointWord = '';
+    if(!empty($point)){
+        $pointWord = numberTowordsForPaisa($point);
+    }
+    $rettxt = $result . "Rupees " . $pointWord;
+    $rettxt .= " Only";
+
+    return ucwords($rettxt);
 }
 
 function IND_money_format($number){
